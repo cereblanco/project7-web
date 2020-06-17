6d0320cd03441d5ca9ff60fccef61955fad81e1d
Cere Blanco <743526+cereblanco@users.noreply.github.com>
2020-06-17 14:42:43 +0800

    Setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    "plugin:@typescript-eslint/recommended", 
+    'plugin:react/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+  },
+  settings: {
+    react: {
+        version: 'detect',
+    },
+  },
+};
