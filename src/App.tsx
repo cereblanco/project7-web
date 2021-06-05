d8faca38828192a3fdaa35bb9ca5b3f3c1f41a06
Cere Blanco <743526+cereblanco@users.noreply.github.com>
2021-06-05 14:58:08 +0800

    Remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import { Box, Container } from "@material-ui/core";
-import CssBaseline from "@material-ui/core/CssBaseline";
 
 import MultipleChoiceDeck from "./pages/MultipleChoiceDeck";
 
