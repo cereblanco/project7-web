4762e6ad4f0109737070fe7e4c7d028ca9b2a485
Cere Blanco <743526+cereblanco@users.noreply.github.com>
2021-06-12 06:37:59 +0800

    Refactor: reduce the number of components in multiple-choice

--- src/components/multiplechoice/Answer.tsx
+++ src/components/multiplechoice/Answer.tsx
@@ -1,20 +1,47 @@
 import { Box, Typography } from "@material-ui/core";
+import { Cancel, CheckCircle } from "@material-ui/icons";
 import React from "react";
 
-export type AnswerProps = {
-  text: string;
-  visible: boolean;
-};
+const cheers: ReadonlyArray<string> = [
+  "Awesome!",
+  "Good job!",
+  "Great!",
+  "Correct!",
+  "You got it right!",
+];
 
-const Answer: React.FC<AnswerProps> = ({ text, visible }: AnswerProps) => {
+type AnswerProps = { isCorrect: boolean; text: string; visible: boolean };
+
+const Answer: React.FC<AnswerProps> = ({
+  isCorrect,
+  text,
+  visible,
+}: AnswerProps) => {
   if (!visible) {
     return null;
   }
+
+  const randomIndex: number = Math.floor(Math.random() * cheers.length);
+
+  if (isCorrect) {
+    return (
+      <>
+        <CheckCircle width="100%" style={{ fontSize: 70 }} color="primary" />
+        <Typography variant="h5">{cheers[randomIndex]}</Typography>
+      </>
+    );
+  }
+
   return (
-    <Box>
-      <Typography variant="subtitle1">ANSWER</Typography>
-      <Typography variant="h4">{text}</Typography>
-    </Box>
+    <>
+      <Box>
+        <Cancel style={{ fontSize: 70 }} color="error" />
+      </Box>
+      <Box>
+        <Typography variant="subtitle1">ANSWER</Typography>
+        <Typography variant="h4">{text}</Typography>
+      </Box>
+    </>
   );
 };
 
--- src/components/multiplechoice/Choices.test.tsx
+++ src/components/multiplechoice/Choices.test.tsx
@@ -4,19 +4,15 @@ import ReactDOM from "react-dom";
 import Choices, { ChoicesProps } from "./Choices";
 
 describe("Choices", () => {
-  let testComponent: React.ReactElement;
   const testProps: ChoicesProps = {
     choices: ["Jose Rizal", "Andres Bonifacio", "Grabriela Silang"],
     onChange: jest.fn(),
+    disabled: false,
   };
-
-  beforeEach(() => {
-    testComponent = <Choices {...testProps} />;
-  });
 
   it("renders without crashing", () => {
     const div = document.createElement("div");
-    ReactDOM.render(testComponent, div);
+    ReactDOM.render(<Choices {...testProps} />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
