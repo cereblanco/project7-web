2432ec7c9a885af4d295d476a5dad587d40ab5d5
Cere Blanco <743526+cereblanco@users.noreply.github.com>
2021-06-12 07:06:52 +0800

    Fix types error

--- src/components/multiplechoice/Answer.test.tsx
+++ src/components/multiplechoice/Answer.test.tsx
@@ -5,27 +5,32 @@ import { render, screen } from "@testing-library/react";
 import Answer, { AnswerProps } from "./Answer";
 
 describe("Answer", () => {
+  let testComponent: React.ReactElement;
   const testProps: AnswerProps = {
     isCorrect: true,
     text: "Correct Answer",
     visible: true,
   };
 
+  beforeEach(() => {
+    testComponent = <Answer {...testProps} />;
+  });
+
   it("renders without crashing", () => {
     const div = document.createElement("div");
-    ReactDOM.render(<Answer {...testProps} />, div);
+    ReactDOM.render(testComponent, div);
     ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it("hides correct answer when isCorrect is true", () => {
+    render(testComponent);
+    expect(screen.queryByText("ANSWER")).not.toBeInTheDocument();
+    expect(screen.queryByText("Correct Answer")).not.toBeInTheDocument();
   });
 
   it("shows correct answer when isCorrect is false", () => {
     render(<Answer {...testProps} isCorrect={false} />);
     expect(screen.getByText("ANSWER")).toBeInTheDocument();
     expect(screen.getByText("Correct Answer")).toBeInTheDocument();
-  });
-
-  it("hides correct answer when isCorrect is true", () => {
-    render(<Answer {...testProps} />);
-    expect(screen.queryByText("ANSWER")).not.toBeInTheDocument();
-    expect(screen.queryByText("Correct Answer")).not.toBeInTheDocument();
   });
 });
--- src/components/multiplechoice/Choices.test.tsx
+++ src/components/multiplechoice/Choices.test.tsx
@@ -1,18 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { render, screen } from "@testing-library/react";
 
 import Choices, { ChoicesProps } from "./Choices";
 
 describe("Choices", () => {
+  let testComponent: React.ReactElement;
   const testProps: ChoicesProps = {
     choices: ["Jose Rizal", "Andres Bonifacio", "Grabriela Silang"],
     onChange: jest.fn(),
     disabled: false,
   };
 
+  beforeEach(() => {
+    testComponent = <Choices {...testProps} />;
+  });
+
   it("renders without crashing", () => {
     const div = document.createElement("div");
-    ReactDOM.render(<Choices {...testProps} />, div);
+    ReactDOM.render(testComponent, div);
     ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it("renders the choices correctly", () => {
+    render(testComponent);
+    const radios = screen.getAllByRole("radio");
+    expect(radios).toHaveLength(testProps.choices.length);
+    testProps.choices.forEach((choice) => {
+      expect(screen.getByText(choice)).toBeInTheDocument();
+    });
   });
 });
